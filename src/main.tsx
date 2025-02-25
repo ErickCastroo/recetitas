feb6d7d715105a5e6cde61e12b38675a679920df
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-02-25 11:34:58 -0700

    feat: replace App component with AppRouter for improved routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { AppRouter } from '@/routes/index.router'
+
 import './index.css'
-import { App } from '@/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppRouter />
   </StrictMode>,
 )
