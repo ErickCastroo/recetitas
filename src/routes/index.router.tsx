50618eae7d333cef4e9432fee7baecea1f789cec
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-02-25 11:33:14 -0700

    feat: add routing with React Router and update package.json for deployment

---
+++ src/routes/index.router.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { Layout } from '@/layout'
+import { Home } from '@/Pages/Home'
+import { DrinksFavoritos } from '@/Pages/DrinksFavoritos'
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/recetitas' element={<Home />} />
+          <Route path='/drinksfavoritos' element={<DrinksFavoritos />} />
+          <Route path='*' element={<div>Not Found</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export { AppRouter }
